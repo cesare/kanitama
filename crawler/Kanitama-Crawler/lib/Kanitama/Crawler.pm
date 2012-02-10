5c235c1ae4ee16c292ede629ebb2d760f6589398
Tatsuro Hisamori <hisamori@fout.jp>
2012-02-11 00:19:53 +0900

    initial commit Kanitama::Crawler

---
+++ crawler/Kanitama-Crawler/lib/Kanitama/Crawler.pm
@@ -0,0 +1,32 @@
+package Kanitama::Crawler;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Kanitama::Crawler -
+
+=head1 SYNOPSIS
+
+  use Kanitama::Crawler;
+
+=head1 DESCRIPTION
+
+Kanitama::Crawler is
+
+=head1 AUTHOR
+
+Tatsuro Hisamori E<lt>myfinder@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
