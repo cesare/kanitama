db68dd22fee096c08fbf211aa1ca0608f30e827d
SAWADA Tadashi <cesare@mayverse.jp>
2012-02-11 00:16:38 +0900

    initial commit

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanitama::Application.config.secret_token = '9bf0bd97dd91db45df14e8677d38abd32652d14bb142e85dbad547003b5c8c1d689c0fd9380323050d9b4899c5458365dcda9b685c47de26e66764854bad5b49'
---
+++ app/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kanitama::Application.config.session_store :cookie_store, key: '_kanitama_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kanitama::Application.config.session_store :active_record_store
