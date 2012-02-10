85300e855d5c8b5f047308394ab575a0c6b12079
SAWADA Tadashi <cesare@mayverse.jp>
2012-02-11 02:46:06 +0900

    enabled to start/stop unicorn

--- app/config/deploy/infrapp.rb
+++ app/config/deploy/infrapp.rb
@@ -6,3 +6,26 @@ set :deploy_to,   "/home/kanitama/app"
 role :web, "kanitama.infrapp.arena.ne.jp"
 role :app, "kanitama.infrapp.arena.ne.jp"
 role :db,  "kanitama.infrapp.arena.ne.jp", :primary => true
+
+set :unicorn_binary, "bundle exec unicorn"
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+  end
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "kill `cat #{unicorn_pid}`"
+  end
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "kill -s USR2 `cat #{unicorn_pid}`"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
+end
